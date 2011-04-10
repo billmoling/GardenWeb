cd1dd49f03f3f0e444cac63f4640504aa1e12c87
billmoling <panan.songyu@gmail.com>
2011-04-10 14:14:21 +0800

    1.add logwrite 2.change customer to customize

---
+++ WebSite/App_Code/Constants.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+
+
+    public struct Priority
+    {
+        public const int Lowest = 0;
+        public const int Low = 1;
+        public const int Normal = 2;
+        public const int High = 3;
+        public const int Highest = 4;
+    }
+
+    public struct Category
+    {
+        public const string General = "General";
+        public const string Trace = "Trace";
+        public const string Exception = "Exception";
+    }
--- WebSite/admin/SideBarUpdate.aspx.cs
+++ WebSite/admin/SideBarUpdate.aspx.cs
@@ -5,9 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
 
 public partial class SideBarUpdate : System.Web.UI.Page
 {
+    private LogWriter logwrite = EnterpriseLibraryContainer.Current.GetInstance<LogWriter>();
     
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -25,9 +28,30 @@ public partial class SideBarUpdate : System.Web.UI.Page
     }
     protected void Btn_Update_Click(object sender, EventArgs e)
     {
-        using (StreamWriter sw = new StreamWriter(Server.MapPath("../SideBar/SideBar.htm"), false, System.Text.Encoding.Unicode))
+        try
         {
-            sw.Write(Txt_SourceCode.Text);
+            using (StreamWriter sw = new StreamWriter(Server.MapPath("../SideBar/SideBar.htm"), false, System.Text.Encoding.Unicode))
+            {
+                sw.Write(Txt_SourceCode.Text);
+            }
+            logwrite.Write(
+                "User Update SideBar Success.",
+                Category.General,
+                Priority.Low
+                );
         }
+        catch (Exception ex)
+        {
+
+            LogEntry entry=new LogEntry ();
+            entry.Severity=System.Diagnostics.TraceEventType.Error;
+            entry.Message=string.Format("Exception:{0}, Inner Exception:{1}", ex.Message, ex.InnerException.Message);
+            entry.Categories.Add(Category.General);
+            entry.Priority=Priority.High;
+            logwrite.Write(entry);
+            throw;
+        }
+
+        
     }
 }
